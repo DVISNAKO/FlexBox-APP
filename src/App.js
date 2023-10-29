472360180c5992cc709e5b8e55807d357748ecfe
Dima <dmitrijs.visnakovs@inbox.lv>
2023-10-29 08:41:42 +0200

    Flex box app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [flexy, setFlexy] = useState('');
+  const [just, setJust] = useState('');
+  const [aligy, setAligy] = useState('');
+
+  const flex = 'flex';
+  const flexNot = 'flexNot-none';
+
+  const justStart = 'justStart';
+  const justCenter = 'justCenter';
+  const justEnd = 'justEnd';
+
+  const aligStart = 'aligStart';
+  const aligCenter = 'aligCenter';
+  const aligEnd = 'aligEnd';
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Flex Box
+      <div className={`${flexy} ${just} ${aligy} output`}>
+        <div className='block'>1</div>
+        <div className='block'>2</div>
+        <div className='block'>3</div>
+      </div>
+    <div className='btn'>
+      <div className='display'>
+      <button onClick={() => setFlexy(flex)}>display: flex</button>
+      <button onClick={() => setFlexy(flexNot)}>display: none</button>
+      </div>
+     
+      <div className='just'>
+      <button onClick={() => setJust(justStart)}>justify-content: flex-start</button>
+      <button onClick={() => setJust(justCenter)}>justify-content: center</button>
+      <button onClick={() => setJust(justEnd)}>justify-content: flex-end</button>
+      </div>
+
+      <div className='align'>
+      <button onClick={() => setAligy(aligStart)}>align-items: flex-start</button>
+      <button onClick={() => setAligy(aligCenter)}>align-items: center</button>
+      <button onClick={() => setAligy(aligEnd)}>align-items: flex-end</button>
+      </div>
+    </div>
     </div>
   );
 }
